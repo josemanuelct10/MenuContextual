ad119e38ee80f4c45a5c14564e696be4ad8d4821
josem <josemanuel30503@hotmail.com>
2022-11-28 10:59:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/menucontextual/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.menucontextual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.example.menucontextual.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imagen = binding.imagen
+        registerForContextMenu(imagen)
+
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_contextual, menu)
+        menu.setHeaderTitle("Menu Contextual")
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.descargar -> {
+                Toast.makeText(this, "Opcion descargar", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.compartir -> {
+                Toast.makeText(this, "Opcion compartir", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.copiar -> {
+                Toast.makeText(this, "Opcion copiar", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+
+        }
+    }
+}
